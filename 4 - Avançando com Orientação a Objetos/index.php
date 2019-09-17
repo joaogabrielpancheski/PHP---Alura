79b2bdb01f8a2cecb0ccc0962baf10950c323e3b
João Gabriel Pancheski <i22111@integrado.int>
2019-09-17 17:54:29 -0300

    Avançando com Orientação a Objetos - Aula 01 - Namespace

---
+++ 4 - Avançando com Orientação a Objetos/index.php
@@ -0,0 +1,13 @@
+<?php
+
+ini_set("display_errors", 1);
+require_once "autoload.php";
+
+use classes\funcionarios\Diretor;
+use classes\funcionarios\Designer;
+
+$diretor = new Diretor();
+$designer = new Designer();
+
+var_dump($diretor);
+
